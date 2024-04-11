684ffe85eba465308cea1863d5aea43511a72621
KeerthiVasan-ai <keerthi77459@gmail.com>
2024-04-11 15:55:32 +0530

    Added Constants Value

---
+++ lib/utils/constants.dart
@@ -0,0 +1,11 @@
+class Constants {
+  static const universityName = "ANNAMALAI UNIVERSITY";
+  static const facultyName = "Faculty of Engineering and Technology";
+  static const deptName = "Dept. of Computer Science and Engineering";
+  static const appName = "Smart Reserve";
+  static const appDescription = "App Developed by CSE Dept.";
+  static const copyright = "Smart Reserve © Dept of CSE, FEAT, AU";
+
+  static List<String> reportFormat = ["PDF","XLSX"];
+
+}
